f9b1b879505540bec08c5f5da5d56e3bd848dd2b
sashida <sashida@primagest.co.jp>
2020-06-10 09:51:00 +0900

    p.1312

--- resolvers/Mutation.js
+++ resolvers/Mutation.js
@@ -1,5 +1,34 @@
+const { authorizeWithGithub } = require('../lib')
 const { ObjectID } = require('mongodb')
 
 module.exports = {
-    
+    async githubAuth(parent, { code }, { db }) {
+        let {
+            message,
+            access_token,
+            avatar_url,
+            login,
+            name
+        } = await authorizeWithGithub({
+            client_id: process.env.CLIENT_ID,
+            client_secret: process.env.CLIENT_SECRET,
+            code
+        })
+
+        if (message) {
+            throw new Error(message)
+        }
+
+        let latestUserInfo = {
+            name,
+            githubLogin: login,
+            githubToken: access_token,
+            avatar: avatar_url
+        }
+
+        const { ops: [user] } = await db
+            .collection('users').replaceOne({ githubLogin: login }, latestUserInfo, { upsert: true })
+
+        return { user, token: access_token }
+    }
 }
